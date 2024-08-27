b5f20540b1bbabb189446a02a7d3694437dfd86c
SteveGrey <greysteve1980@gmail.com>
2024-08-27 20:51:58 +0100

    initial commit & cleanup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,17 +6,9 @@ import { ScreenContent } from '~/components/ScreenContent';
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: 'Google Photos' }} />
+      
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
